6bf58f8a850393e076c92fc4f885b94982833169
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-11 23:41:51 +0200

    Spawn new character command

---
+++ src/CharacterApi/Application/CharacterLocations/SpawnCharacter/SpawnCharacterCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+
+namespace CharacterApi.Application.CharacterLocations.SpawnCharacter
+{
+    public class SpawnCharacterCommand : IRequest<CharacterLocationDto>
+    {
+        public Guid CharacterId { get; }
+
+        public SpawnCharacterCommand(Guid characterId)
+        {
+            CharacterId = characterId;
+        }
+    }
+}
