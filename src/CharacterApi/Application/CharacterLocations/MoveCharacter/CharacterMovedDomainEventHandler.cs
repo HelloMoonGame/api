7660c5bd84b3760dce234a86a6cb81e89b0d6184
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-11 23:39:19 +0200

    Add CharacterLocation queries

---
+++ src/CharacterApi/Application/CharacterLocations/MoveCharacter/CharacterMovedDomainEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CharacterApi.Domain.CharacterLocations;
+using CharacterApi.GrpcServices;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CharacterApi.Application.CharacterLocations.IntegrationHandlers
+{
+    public class CharacterMovedDomainEventHandler : INotificationHandler<CharacterMovedEvent>
+    {
+        private readonly ILogger<CharacterMovedDomainEventHandler> _logger;
+
+        public CharacterMovedDomainEventHandler(ILogger<CharacterMovedDomainEventHandler> logger)
+        {
+            _logger = logger;
+        }
+        
+        public Task Handle(CharacterMovedEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Character {notification.CharacterId} moved to {notification.ToX},{notification.ToY}");
+            LocationService.PublishCharacterLocation(notification.CharacterId, new CharacterLocationDto
+            {
+                CharacterId = notification.CharacterId,
+                X = notification.ToX,
+                Y = notification.ToY
+            }, cancellationToken);
+            
+            return Task.CompletedTask;
+        }
+    }
+}
