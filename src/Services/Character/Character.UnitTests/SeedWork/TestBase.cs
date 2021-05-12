99826aac495b9ff91a904717a2e4fa6ae78ff22c
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-12 17:38:19 +0200

    Add unit tests for Create Character

---
+++ src/Services/Character/Character.UnitTests/SeedWork/TestBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CharacterApi.Domain.SeedWork;
+using CharacterApi.Domain.SharedKernel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Character.UnitTests.SeedWork
+{
+    public abstract class TestBase : IDisposable
+    {
+        private bool _disposed;
+
+        public static T AssertPublishedDomainEvent<T>(Entity aggregate) where T : IDomainEvent
+        {
+            var domainEvent = DomainEventsTestHelper.GetAllDomainEvents(aggregate).OfType<T>().SingleOrDefault();
+
+            if (domainEvent == null)
+            {
+                throw new Exception($"{typeof(T).Name} event not published");
+            }
+
+            return domainEvent;
+        }
+
+        public static List<T> AssertPublishedDomainEvents<T>(Entity aggregate) where T : IDomainEvent
+        {
+            var domainEvents = DomainEventsTestHelper.GetAllDomainEvents(aggregate).OfType<T>().ToList();
+
+            if (!domainEvents.Any())
+            {
+                throw new Exception($"{typeof(T).Name} event not published");
+            }
+
+            return domainEvents;
+        }
+
+        public static void AssertBrokenRule<TRule>(Action testDelegate) where TRule : class, IBusinessRule
+        {
+            var message = $"Expected {typeof(TRule).Name} broken rule";
+            var businessRuleValidationException = Assert.ThrowsException<BusinessRuleValidationException>(testDelegate, message);
+            if (businessRuleValidationException != null)
+            {
+                Assert.IsInstanceOfType(businessRuleValidationException.BrokenRule, typeof(TRule), message);
+            }
+        }
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+                SystemClock.Reset();
+
+            _disposed = true;
+        }
+    }
+}
