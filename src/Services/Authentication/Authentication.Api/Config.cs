f097391a54f38a3732ec2ec00590d999157ccebc
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-14 14:04:56 +0200

    Show pretty text on consent page and only publish email instead of profile

--- src/Services/Authentication/Authentication.Api/Config.cs
+++ src/Services/Authentication/Authentication.Api/Config.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using IdentityServer4.Models;
 
 namespace Authentication.Api
@@ -6,17 +7,30 @@ namespace Authentication.Api
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
+            new IdentityResource[]
+            {
                 new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
+                new IdentityResources.Email { Required = true},
+            };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ("characterapi"),
+                new("characterapi")
+                {
+                    DisplayName = "Characters", Required = true, Description = "Gives access to control your character and view others."
+                },
             };
+
+        private static ICollection<string> AllScopes
+        {
+            get
+            {
+                var scopes = ApiScopes.Select(s => s.Name).ToList();
+                scopes.AddRange(IdentityResources.Select(r => r.Name));
+                return scopes;
+            }
+        }
 
         public static IEnumerable<Client> Clients(string gameUrl) =>
             new []
@@ -24,6 +38,7 @@ namespace Authentication.Api
                 new Client
                 {
                     ClientId = "game",
+                    ClientName = "Game",
                     ClientSecrets = { new Secret("39861364-42fc-4147-b3aa-88248a00bc1b".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.Code,
@@ -31,9 +46,25 @@ namespace Authentication.Api
 
                     RedirectUris = { gameUrl + "/auth/signin-callback", gameUrl + "/auth/renew-callback" },
                     
-                    AllowedScopes = { "openid", "profile", "characterapi" },
+                    AllowedScopes = AllScopes,
                     
                     RequireConsent = false
+                },
+                new Client
+                {
+                    ClientId = "docs",
+                    ClientName = "Documentation",
+                    ClientSecrets = { new Secret("5b7f084b-9eb4-4f0b-a833-e6a32b9a51bd".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+
+                    RedirectUris = { "https://docs.hellomoon.nl/auth/signin-callback", 
+                        "https://docs.hellomoon.nl/auth/renew-callback" },
+
+                    AllowedScopes = AllScopes,
+
+                    RequireConsent = true
                 },
             };
     }
