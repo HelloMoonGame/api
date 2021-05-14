d5a77a81d5bcbef9bfeca5aac1a5521a72c5ffab
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-14 20:47:59 +0200

    Remove unused code

--- src/Services/Authentication/Authentication.Api/Quickstart/Account/LoggedOutViewModel.cs
+++ src/Services/Authentication/Authentication.Api/Quickstart/Account/LoggedOutViewModel.cs
@@ -1,7 +1,3 @@
-// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
 namespace Authentication.Api.Quickstart.Account
 {
     public class LoggedOutViewModel
@@ -9,7 +5,6 @@ namespace Authentication.Api.Quickstart.Account
         public string PostLogoutRedirectUri { get; set; }
         public string ClientName { get; set; }
         public string SignOutIframeUrl { get; set; }
-
         public string LogoutId { get; set; }
     }
 }
--- src/Services/Authentication/Authentication.Api/Quickstart/Consent/ConsentController.cs
+++ src/Services/Authentication/Authentication.Api/Quickstart/Consent/ConsentController.cs
@@ -104,7 +104,7 @@ namespace Authentication.Api.Quickstart.Consent
             ConsentResponse grantedConsent = null;
 
             // user clicked 'no' - send back the standard 'access_denied' response
-            if (model?.Button == "no")
+            if (model.Button == "no")
             {
                 grantedConsent = new ConsentResponse { Error = AuthorizationError.AccessDenied };
 
@@ -112,16 +112,12 @@ namespace Authentication.Api.Quickstart.Consent
                 await _events.RaiseAsync(new ConsentDeniedEvent(User.GetSubjectId(), request.Client.ClientId, request.ValidatedResources.RawScopeValues));
             }
             // user clicked 'yes' - validate the data
-            else if (model?.Button == "yes")
+            else if (model.Button == "yes")
             {
                 // if the user consented to some scope, build the response model
                 if (model.ScopesConsented != null && model.ScopesConsented.Any())
                 {
                     var scopes = model.ScopesConsented;
-                    if (ConsentOptions.EnableOfflineAccess == false)
-                    {
-                        scopes = scopes.Where(x => x != IdentityServer4.IdentityServerConstants.StandardScopes.OfflineAccess);
-                    }
 
                     grantedConsent = new ConsentResponse
                     {
@@ -206,10 +202,6 @@ namespace Authentication.Api.Quickstart.Consent
                     apiScopes.Add(scopeVm);
                 }
             }
-            if (ConsentOptions.EnableOfflineAccess && request.ValidatedResources.Resources.OfflineAccess)
-            {
-                apiScopes.Add(GetOfflineAccessScope(vm.ScopesConsented.Contains(IdentityServer4.IdentityServerConstants.StandardScopes.OfflineAccess) || model == null));
-            }
             vm.ApiScopes = apiScopes;
 
             return vm;
@@ -244,18 +236,6 @@ namespace Authentication.Api.Quickstart.Consent
                 Emphasize = apiScope.Emphasize,
                 Required = apiScope.Required,
                 Checked = check || apiScope.Required
-            };
-        }
-
-        private ScopeViewModel GetOfflineAccessScope(bool check)
-        {
-            return new ScopeViewModel
-            {
-                Value = IdentityServer4.IdentityServerConstants.StandardScopes.OfflineAccess,
-                DisplayName = ConsentOptions.OfflineAccessDisplayName,
-                Description = ConsentOptions.OfflineAccessDescription,
-                Emphasize = true,
-                Checked = check
             };
         }
     }
