959a6aa7836c24a43622569834fe3e072a4dfb6b
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-20 20:53:12 +0200

    Style wait-for-approval-page

--- src/Services/Authentication/Authentication.Api/wwwroot/js/app.js
+++ src/Services/Authentication/Authentication.Api/wwwroot/js/app.js
@@ -1,1 +1,13 @@
-﻿mdc.textField.MDCTextField.attachTo(document.querySelector('.mdc-text-field'));
+﻿var textFields = document.querySelectorAll('.mdc-text-field');
+
+textFields.forEach(function (textField) {
+    mdc.textField.MDCTextField.attachTo(textField);
+});
+
+var circularProgresses = document.querySelectorAll('.mdc-circular-progress');
+circularProgresses.forEach(function (circularProgress) {
+    var m = mdc.circularProgress.MDCCircularProgress.attachTo(circularProgress);
+    circularProgress.setProgress = function (value) {
+        m.progress = value;
+    };
+});
