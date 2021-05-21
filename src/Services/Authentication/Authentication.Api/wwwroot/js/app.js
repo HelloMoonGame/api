019ec871ea28e68f9a0970ef5c3dadbb870a1306
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-21 12:14:06 +0200

    New design for login page (#17)

---
+++ src/Services/Authentication/Authentication.Api/wwwroot/js/app.js
@@ -0,0 +1,13 @@
+﻿var textFields = document.querySelectorAll('.mdc-text-field');
+
+textFields.forEach(function (textField) {
+    mdc.textField.MDCTextField.attachTo(textField);
+});
+
+var circularProgresses = document.querySelectorAll('.mdc-circular-progress');
+circularProgresses.forEach(function (circularProgress) {
+    var m = mdc.circularProgress.MDCCircularProgress.attachTo(circularProgress);
+    circularProgress.setProgress = function (value) {
+        m.progress = value;
+    };
+});
--- src/Services/Authentication/Authentication.Api/wwwroot/js/login-approval.js
+++ src/Services/Authentication/Authentication.Api/wwwroot/js/login-approval.js
@@ -11,13 +11,18 @@
 }
 
 var expiryDate = new Date($('input#ExpiryDate').val());
+var originalTimeLeft = (expiryDate - new Date()) / 1000;
+var timeLeftElement = document.getElementById('timeLeft');
+
 function updateTimeLeft() {
-    var timeLeft = (expiryDate - new Date()) / 1000,
+    var timeLeft = Math.max(0, (expiryDate - new Date()) / 1000),
         secondsLeft = parseInt(timeLeft % 60),
         minutesLeft = parseInt(timeLeft / 60);
 
-    $('#timeLeft').html(minutesLeft + ":" + ("0" + secondsLeft).slice(-2));
-
+    var progress = timeLeft / originalTimeLeft;
+    timeLeftElement.setProgress(progress);
+    timeLeftElement.getElementsByClassName("text")[0].innerText = minutesLeft + ":" + ("0" + secondsLeft).slice(-2);
+    
     setTimeout(updateTimeLeft, 1000);
 }
 
