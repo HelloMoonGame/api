aa0cd0fbbfeee4e7f2908dcc400ee0138e927e6d
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-21 22:01:48 +0200

    Add integration tests

--- src/Services/Authentication/Authentication.IntegrationTests/Controllers/ConsentControllerTests.cs
+++ src/Services/Authentication/Authentication.IntegrationTests/Controllers/ConsentControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 using AngleSharp.Html.Dom;
 using Authentication.Api.Models.Email;
@@ -26,6 +27,21 @@ namespace Authentication.IntegrationTests.Controllers
             Assert.AreEqual(HttpStatusCode.OK, returnResult.StatusCode);
             var requestUrl = returnResult.RequestMessage?.RequestUri?.ToString() ?? "";
             Assert.IsTrue(requestUrl.StartsWith("http://localhost/consent"), $"User was redirected to {requestUrl} instead of http://localhost/consent");
+        }
+        
+        [TestMethod]
+        public async Task Error_is_shown_when_return_url_does_not_belong_to_a_third_party_app()
+        {
+            // Arrange
+            await StartLogin(); // user is now authenticated
+
+            // Act
+            var returnResult = await Client.GetAsync("/Consent?ReturnUrl=https://microsoft.com");
+            var returnContent = await returnResult.Content.ReadAsStringAsync();
+            // Assert
+            Assert.AreEqual(HttpStatusCode.InternalServerError, returnResult.StatusCode);
+            var searchFor = "Sorry, there was an error";
+            Assert.IsTrue(returnContent.Contains(searchFor), $"Page should contain {searchFor}, but is:\n {returnContent}");
         }
 
         [TestMethod]
@@ -60,6 +76,28 @@ namespace Authentication.IntegrationTests.Controllers
             // Assert
             Assert.IsTrue(allowConsentResult.RequestMessage?.RequestUri?.ToString().StartsWith("https://localhost:5001") ?? false, $"User is forwarded to {allowConsentResult.RequestMessage?.RequestUri}, while it was expected to go to https://localhost:5001");
             Assert.IsTrue(allowConsentResult.RequestMessage.RequestUri.ToString().Contains("error=access_denied"), $"User is forwarded to {allowConsentResult.RequestMessage.RequestUri}, while the url should contain 'error=access_denied'.");
+        }
+        
+        [TestMethod]
+        public async Task Error_is_shown_when_return_url_is_no_longer_valid_when_giving_consent()
+        {
+            // Arrange
+            var loginAttemptStatusResult = await StartLogin();
+            var returnResult = await Client.GetAsync(loginAttemptStatusResult.ReturnUrl);
+            var consentDocument = await returnResult.GetDocumentAsync();
+
+            // Act
+            var form = consentDocument.Forms["consent"];
+            var allowConsentResult = await Client.SendAsync(form, form.QuerySelector("button[value='yes']") as IHtmlElement, new KeyValuePair<string, string>[]
+            {
+                new ("ReturnUrl", "https://microsoft.com")
+            });
+            var allowConsentContent = await allowConsentResult.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.InternalServerError, allowConsentResult.StatusCode);
+            var searchFor = "Sorry, there was an error";
+            Assert.IsTrue(allowConsentContent.Contains(searchFor), $"Page should contain {searchFor}, but is:\n {allowConsentContent}");
         }
 
         private async Task<LoginHelpers.LoginAttemptStatusResult> StartLogin()
