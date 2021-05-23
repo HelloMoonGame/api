6ac80b9cbc2a91bfaddfa0bc06401fa38654a2bb
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-23 23:08:21 +0200

    Add logging via Serilog

--- src/Services/Character/Character.Api/Program.cs
+++ src/Services/Character/Character.Api/Program.cs
@@ -1,5 +1,8 @@
+using System;
+using Character.Api.Configuration;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Character.Api
 {
@@ -7,15 +10,35 @@ namespace Character.Api
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            host.Run();
+            LoggingExtensions.SetupLoggerConfiguration(AppName, AppVersion);
+
+            try
+            {
+                Log.Information("Starting web host for {0}", AppName);
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((hostBuilderContext, loggerConfiguration) => {
+                    loggerConfiguration.ConfigureBaseLogging(
+                        hostBuilderContext.Configuration, AppName, AppVersion);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        
+        public static string AppName => typeof(Program).Assembly.GetName().Name;
+        public static Version AppVersion => typeof(Program).Assembly.GetName().Version;
     }
 }
