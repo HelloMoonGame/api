7ca340d921244201093d119dcf537b6b17cb881d
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-23 16:45:05 +0200

    Recover deleted JavaScript

--- src/Services/Authentication/Authentication.Api/wwwroot/js/app.js
+++ src/Services/Authentication/Authentication.Api/wwwroot/js/app.js
@@ -1,4 +1,14 @@
-﻿var circularProgresses = document.querySelectorAll('.mdc-circular-progress');
+﻿var textFields = document.querySelectorAll('.mdc-text-field');
+textFields.forEach(function (textField) {
+    mdc.textField.MDCTextField.attachTo(textField);
+});
+
+var lists = document.querySelectorAll('.mdc-list');
+lists.forEach(function (list) {
+    mdc.list.MDCList.attachTo(list);
+});
+
+var circularProgresses = document.querySelectorAll('.mdc-circular-progress');
 circularProgresses.forEach(function (circularProgress) {
     var m = mdc.circularProgress.MDCCircularProgress.attachTo(circularProgress);
     circularProgress.setProgress = function (value) {
