769d5a58b3365fa0f89cbfe5dbea77950da4da44
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-31 18:23:25 +0200

    Use database storage for Authentication API instead of in-memory databases (#23)

--- src/Services/Authentication/Authentication.Api/SeedData.cs
+++ src/Services/Authentication/Authentication.Api/SeedData.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Linq;
 using Authentication.Api.Data;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Authentication.Api
@@ -10,11 +15,44 @@ namespace Authentication.Api
         public static void EnsureSeedData(IServiceProvider serviceProvider)
         {
             using var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            var configuration = scope.ServiceProvider.GetService<IConfiguration>();
 
-            var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
-            MigrateDatabase(context);
+            var applicationDbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+            MigrateDatabase(applicationDbContext);
+            
+            var configurationDbContext = scope.ServiceProvider.GetService<ConfigurationDbContext>();
+            MigrateDatabase(configurationDbContext);
+            
+            var persistedGrantDbContext = scope.ServiceProvider.GetService<PersistedGrantDbContext>();
+            MigrateDatabase(persistedGrantDbContext);
+            
+            foreach (var client in Config.Clients(configuration["GameUrl"],
+                configuration["CharacterApiUrl"]))
+            {
+                if (configurationDbContext.Clients.All(c => c.ClientId != client.ClientId))
+                {
+                    var corsUri = new Uri(client.RedirectUris.First());
+                    client.AllowedCorsOrigins.Add(corsUri.Scheme + "://" + corsUri.Authority);
+                    configurationDbContext.Clients.Add(client.ToEntity());
+                }
+            }
+            configurationDbContext.SaveChanges();
+            
+            foreach (var resource in Config.IdentityResources)
+            {
+                if (configurationDbContext.IdentityResources.All(r => r.Name != resource.Name))
+                    configurationDbContext.IdentityResources.Add(resource.ToEntity());
+            }
+            configurationDbContext.SaveChanges();
+
+            foreach (var apiScope in Config.ApiScopes)
+            {
+                if (configurationDbContext.ApiScopes.All(s => s.Name != apiScope.Name))
+                    configurationDbContext.ApiScopes.Add(apiScope.ToEntity());
+            }
+            configurationDbContext.SaveChanges();
         }
-        private static void MigrateDatabase(ApplicationDbContext context)
+        private static void MigrateDatabase(DbContext context)
         {
             if (context == null)
                 throw new ArgumentNullException(nameof(context), "Database dependency could not be resolved");
