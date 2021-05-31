f6b3b8ab431e0a6de609bcb336adb5859747ed90
Vincent Bitter <vincent@vincentbitter.nl>
2021-05-31 15:19:05 +0200

    Create self-signed certificate and update once expired

--- src/Services/Authentication/Authentication.Api/Startup.cs
+++ src/Services/Authentication/Authentication.Api/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Authentication.Api.Configuration;
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Authentication.Api.Configuration;
 using Authentication.Api.Data;
 using Authentication.Api.Domain.Login;
 using Authentication.Api.Infrastructure;
@@ -6,6 +10,7 @@ using Authentication.Api.Infrastructure.Domain.Login;
 using Authentication.Api.Models;
 using Authentication.Api.Services;
 using Common.Domain.SeedWork;
+using Common.Domain.SharedKernel;
 using Common.Infrastructure.Domain;
 using Common.Infrastructure.Processing;
 using IdentityServer4.Services;
@@ -18,6 +23,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Org.BouncyCastle.Security.Certificates;
+using Serilog;
 
 namespace Authentication.Api
 {
@@ -72,8 +79,14 @@ namespace Authentication.Api
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients(Configuration["GameUrl"], Configuration["CharacterApiUrl"]))
                 .AddAspNetIdentity<ApplicationUser>();
-            
-            builder.AddDeveloperSigningCredential();
+
+            var certificateFile = Path.Combine(".", "certs", "certificate.pfx");
+            var directoryInfo = new FileInfo(certificateFile).Directory;
+            if (directoryInfo != null)
+                Directory.CreateDirectory(directoryInfo.FullName);
+            var certificate = LoadOrCreateCertificate(certificateFile);
+            builder.AddSigningCredential(certificate)
+                .AddValidationKey(certificate);
 
             services.AddAuthentication();
 
@@ -97,6 +110,48 @@ namespace Authentication.Api
                     opt.EnableActiveTelemetryConfigurationSetup = true;
                 });
             }
+        }
+
+        private X509Certificate2 LoadOrCreateCertificate(string certificateFile)
+        {
+            X509Certificate2 certificate;
+            try
+            {
+                certificate = new X509Certificate2(certificateFile);
+                if (certificate.NotBefore.ToUniversalTime() > SystemClock.Now ||
+                    certificate.NotAfter.ToUniversalTime() < SystemClock.Now)
+                {
+                    Log.Logger.Warning("Certificate only valid between {ValidFrom} and {ValidTill}", 
+                        certificate.NotBefore.ToUniversalTime(), 
+                        certificate.NotAfter.ToUniversalTime());
+                    throw new CertificateExpiredException();
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Warning(e, "Creating new certificate");
+                certificate = GenerateSelfSignedCertificate();
+                File.WriteAllBytes(certificateFile, certificate.Export(X509ContentType.Pfx));
+            }
+
+            return certificate;
+        }
+
+        public static X509Certificate2 GenerateSelfSignedCertificate()
+        {
+            X500DistinguishedName distinguishedName = new X500DistinguishedName("CN=Authentication");
+
+            using RSA rsa = RSA.Create(2048);
+            var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+            request.CertificateExtensions.Add(
+                new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
+
+            request.CertificateExtensions.Add(
+                new X509EnhancedKeyUsageExtension(
+                    new OidCollection { new ("1.3.6.1.5.5.7.3.2") }, false));
+
+            return request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddYears(10)));
         }
 
         protected virtual void AddDatabase(IServiceCollection services)
